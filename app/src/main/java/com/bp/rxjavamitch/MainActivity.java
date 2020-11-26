813bc1e5eae6230ee96ef34ddcd1ffe8dddd2cdc
behrooz pahlavan <pahlavan111@gmail.com>
2020-11-27 00:09:06 +0330

    ADD Task class

--- app/src/main/java/com/bp/rxjavamitch/MainActivity.java
+++ app/src/main/java/com/bp/rxjavamitch/MainActivity.java
@@ -3,12 +3,19 @@ package com.bp.rxjavamitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "bp";
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text=findViewById(R.id.text);
     }
 }
